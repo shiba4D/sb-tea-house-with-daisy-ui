9fea2f01f076fe48f6bc7930e96fac552f1ec244
unknown <>
2025-04-13 00:48:25 +0600

    created layout with gradient

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwind.config.js') .Config} */
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
